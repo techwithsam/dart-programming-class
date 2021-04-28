c6e472856b907f2ad2aaab8922becabd4991ef29
Samuel Adekunle <samuelbeebest@gmail.com>
2021-04-28 12:10:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# dart-programming-class" 
---
+++ main.dart
@@ -0,0 +1,7 @@
+main() {
+  String name = "Samuel Adekule";
+  int age = 18;
+  bool student = true;
+  List<int> score = [10, 20, 30];
+  print("My name is $name, am $age years old, Student = $student, scores" + score.toString());
+}
