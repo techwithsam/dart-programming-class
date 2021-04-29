51a1c27b13b56eb7788e67bfd2a5a88b2b663f30
Samuel Adekunle <samuelbeebest@gmail.com>
2021-04-29 14:40:27 +0100

    expression(operand & operators)

---
+++ expression.dart
@@ -0,0 +1,46 @@
+// Expression -- Operand & Operators
+
+// *******Arithmetic Operators********
+// + addition
+// - subtration
+// / division
+// ~/ divide, returning an integer result
+// % get the remainder of an integer division(modulo)
+// ++ Increment
+// -- Decrement
+//
+// ***** Equality and Relational
+// > Greater than
+// < Less than
+// >= Greater than or equal to
+// <= Less than or equal to
+// == Equality
+// != Not equal
+//
+
+main() {
+  int number1 = 25;
+  int number2 = 300;
+  bool condit = true;
+  if (number2 == 1 || number1 != 25) {
+    print('true decision');  
+  } else if (condit != true) {
+    print('Not true');
+  } else {}
+
+  String name = 'Jide';
+  if (name is! String)
+    print('Data Type is String');
+  else
+    print('Type is not String');
+}
+
+// **** Type Test *******
+// is True if the object has the specified type
+// is! False if the object has the specified type
+
+// **** Logical Operators ****
+// && And
+// || OR
+// ! Not 
+
