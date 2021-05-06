ae042060635e10f3689a5b2e953d7aa90c834728
Samuel Adekunle <samuelbeebest@gmail.com>
2021-05-06 15:03:49 +0100

    classes in dart

---
+++ classes.dart
@@ -0,0 +1,57 @@
+// Classes & Objects in Dart
+// Class class_name {
+//    <fields>
+//    <getter> / <setter>
+//    <contructor>
+//    <function>
+// }
+import 'students.dart';
+
+main() {
+  Teacher tech = Teacher();
+  tech.hostel;
+  School sch = School();
+  print(sch);
+  print(sch.students);
+}
+
+// parent class School
+class School {
+  // final String stuName;
+  // School({required this.stuName});
+
+  // School(String stuName) {
+  //   print(stuName);
+  // }
+
+  // field
+  String students = "145";
+
+  // function
+  void dis() {
+    //  print(stuName);
+    print(students);
+  }
+
+  // Setter & Getter in Dart
+  late String stuSubj;
+
+  // Declaring our Getter
+  // Syntax
+  // return_type get indentifiers {}
+  // set indentifiers {}
+
+  // get example
+  String get subject {
+    print('Get: $stuSubj');
+    return stuSubj;
+  }
+
+  // set example
+  void set subject(stuSubj) {
+    this.stuSubj = stuSubj;
+  }
+}
+
+// extending a parent class to  new sub class
+class Teacher extends Students {}
---
+++ students.dart
@@ -0,0 +1,6 @@
+class Students {
+   String name = 'Ojedokun Adelanwa';
+   String course = 'Art';
+   String level = '400l';
+   String hostel = 'No 23, ajelenkok str. Iorin';
+}
