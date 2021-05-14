06f7503398dd11614f9928abee1e33cd7e86c4ea
Samuel Adekunle <samuelbeebest@gmail.com>
2021-05-14 14:58:25 +0100

    Dart Excersice

---
+++ test.dart
@@ -0,0 +1,35 @@
+// Dart Class Test
+void findError(int a, int b) {
+  print(a * b);
+}
+
+var carName = "Volvo";
+var x = 50;
+
+main() {
+  // Variable
+  var x = 5;
+  var y = 10;
+  var z = x + y;
+  print(x + y);
+  print(z);
+  var firstName = 'John', lastName = 'Doe', age = 35;
+  print(firstName);
+  print(lastName);
+  print(age);
+
+  // Operators
+  print(10 * 5);
+  print(10 / 2);
+  print(15 % 9);
+  print(x += y);
+  print(x *= y);
+
+  var person = {'firstName': 'John', 'lastName': 'Doe'};
+
+  print(person['firstName']);
+}
+
+void myFunction() {
+  print("Hello World");
+}
